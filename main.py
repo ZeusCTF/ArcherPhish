32e8a4dc5cff0d9e9585d171a49bf6be891b2675
Barry Allen <barryallen@Barrys-MacBook-Air.local>
2022-09-02 07:52:14 -0400

    Adding some socket functionality

--- main.py
+++ main.py
@@ -4,24 +4,30 @@ Draft #1:
 Using Flask to handle HTTP requests
 
 """
-import re
+import socket
 from flask import Flask, request
 
 app = Flask(__name__)
 
-def proxyHandle():
+def proxyHandle(path):
     host = request.headers.get('Host')
     method = request.method
-
-
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((f"{host}",80))
+    req = f"{method} /{path} HTTP/1.1\r\nHost: {host}\r\n\r\n"
+    s.sendall(str.encode(req))
+    resp = s.recv(4096)
+    s.close
+    return resp
+    
 
 
 #reqPath functions as a dynamic var that allows any page to be requested
 #instead of the alternative of creating a route for every single page that could be on a remote site
 @app.route("/<reqPath>", methods=['GET','POST'])
 def render(reqPath):
-    proxyHandle()
-    return "<h1>Hello World</h1>"
+    return proxyHandle(reqPath)
+    
 
 @app.route('/')
 def main():
